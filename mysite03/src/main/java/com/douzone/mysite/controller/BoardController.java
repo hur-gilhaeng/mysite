a70a673c028efbfe6f0875ac6cda1cfdbe78ce8d
R35 <R35@DESKTOP-J04NN5U>
2020-03-20 21:52:28 +0900

    보드주석수정

--- mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -1,16 +1,16 @@
 package com.douzone.mysite.controller;
 
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 import com.douzone.mysite.service.BoardService;
 import com.douzone.mysite.vo.BoardVo;
