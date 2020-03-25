ed20b4a9e63f89fef3d2b5ca682957afb3e1bb8e
R35 <R35@DESKTOP-J04NN5U>
2020-03-25 15:48:58 +0900

    FileUploadConfig

--- mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
+import com.douzone.mysite.config.web.FileUploadConfig;
 import com.douzone.mysite.config.web.MessageConfig;
 import com.douzone.mysite.config.web.MvcConfig;
 import com.douzone.mysite.config.web.SecurityConfig;
@@ -12,6 +13,6 @@ import com.douzone.mysite.config.web.SecurityConfig;
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({"com.douzone.mysite.controller","com.douzone.mysite.exception"})
-@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class})
+@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
 public class WebConfig {
 }
