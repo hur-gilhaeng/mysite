44799737e134495cc2a35464930cce846bd5f687
R35 <R35@DESKTOP-J04NN5U>
2020-03-27 20:56:27 +0900

    mysite06-adminMain

--- mysite06/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite06/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -1,20 +1,48 @@
 package com.douzone.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.douzone.mysite.service.AdminService;
+import com.douzone.mysite.vo.SiteVo;
 import com.douzone.security.Auth;
 
 @Auth("ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
 
 	@RequestMapping("")
-	public String main() {
+	public String main(Model model) {
+		
+		SiteVo siteVo = adminService.select();
+		model.addAttribute("siteVo",siteVo);
+		
 		return "admin/main";
 	}
 	
+	@RequestMapping(value = "/main/update", method = RequestMethod.POST)
+	public String mainUpdate(
+			SiteVo siteVo, 
+			@RequestParam(value="file") MultipartFile multipartFile) {
+		
+		if(!multipartFile.isEmpty()) {
+			String url = adminService.restore(multipartFile);
+			siteVo.setProfile(url);
+		}
+		System.out.println(siteVo);
+		adminService.update(siteVo);
+		
+		return "redirect:/admin";
+	}
 
 	@RequestMapping("/guestbook")
 	public String guestbook() {
