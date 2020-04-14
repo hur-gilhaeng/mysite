f9de6ee3bb86d71260011166539839e5b01be1a2
hur-gilhaeng <hgh8401@gmail.com>
2020-04-14 15:12:31 +0900

    mysitr05-messageconverter

--- mysite05/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite05/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -1,8 +1,5 @@
 package com.douzone.mysite.repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,29 +8,35 @@ import com.douzone.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
+	
 	@Autowired
 	private SqlSession sqlSession;
 	
 	public int insert(UserVo vo) {
+
 		return sqlSession.insert("user.insert", vo);
 	}
-
-	public UserVo findByEmailAndPassword(UserVo vo) {
-		return sqlSession.selectOne("user.findByEmailAndPassword", vo);
+	
+	public UserVo findByEmailAndPassword(UserVo inputVo) {
+		
+		UserVo result = sqlSession.selectOne("user.findByEmailAndPassword",inputVo);
+		
+		return result;
 	}
 	
-	public UserVo findByEmailAndPassword(String email, String password) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("e", email);
-		map.put("p", password);
-		return sqlSession.selectOne("user.findByEmailAndPassword2", map);
+	public UserVo findByNo(Long no) {
+		
+		UserVo result = sqlSession.selectOne("user.findByNo",no);
+		
+		return result;
+	}
+	
+	public int uptate(UserVo vo) {
+		return sqlSession.update("user.update", vo);
 	}
 
-	public UserVo find(Long no) {
-		return sqlSession.selectOne("user.find", no);
+	public UserVo findByEmail(String email) {
+		return sqlSession.selectOne("user.findByEmail", email);
 	}
 
-	public int update(UserVo userVo) {
-		return sqlSession.update("user.update", userVo);
-	}
 }
