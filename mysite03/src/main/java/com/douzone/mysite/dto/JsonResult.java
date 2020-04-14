474309fb0ee79d619936b39a2ca05983193dd3d3
hur-gilhaeng <hgh8401@gmail.com>
2020-04-14 13:33:51 +0900

    mysite03 - 편집
    json exception 응답 & form validation

--- mysite03/src/main/java/com/douzone/mysite/dto/JsonResult.java
+++ mysite03/src/main/java/com/douzone/mysite/dto/JsonResult.java
@@ -27,19 +27,10 @@ public class JsonResult {
 	public String getResult() {
 		return result;
 	}
-	public void setResult(String result) {
-		this.result = result;
-	}
 	public Object getData() {
 		return data;
 	}
-	public void setData(Object data) {
-		this.data = data;
-	}
 	public String getMessage() {
 		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
 	}
 }
--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.douzone.mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -11,6 +12,8 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.douzone.mysite.dto.JsonResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,20 +27,33 @@ public class GlobalExceptionHandler {
 			Exception e) throws Exception {
 		
 		// 1. 로깅(logging)
-		StringWriter errors = new StringWriter();
+		StringWriter errors = new StringWriter(); // 버퍼
 		e.printStackTrace(new PrintWriter(errors));
-		
-		LOG.error(errors.toString());
+		LOG.error(errors.toString()); 
 		// LOGGER.error(errors.toString());
 		
+		// 2. 요청구분
+		// 만약, JSON 요청인 경우에는 request header의 Accpet에 application/json
+		// 만약, HTML 요청인 경우에는 request header의 Accpet에 text/html
+		// 만약, jpeg 요청인 경우에는 request header의 Accpet에 image/jpeg 
+		String accept = request.getHeader("accept");
 		
-		// 2. 안내페이지 가기(정상종료)
-		request.setAttribute(
-				"exception", 
-				errors.toString());
-		
-		request.getRequestDispatcher(
-				"/WEB-INF/views/error/exception.jsp")
-					.forward(request, response);
+		if(accept.matches(".*application/json.*")) {
+			// JSON 응답
+			response.setStatus(HttpServletResponse.SC_OK);
+			
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+			
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("UTF-8"));
+			os.close();
+		} else {
+			// 3. 안내페이지 가기(정상종료)
+			request.setAttribute("exception", errors.toString());
+			request
+				.getRequestDispatcher("/WEB-INF/views/error/exception.jsp")
+				.forward(request, response);
+		}
 	}
 }
